7bd4476767315d124ea50b033f64d36b0ba3bcae
David Sherlock <davidmichaelsherlock@icloud.com>
2024-12-18 01:37:00 +0300

    Refactoring

--- src/Client.php
+++ src/Client.php
@@ -2,7 +2,7 @@
 /**
  * Google Geocoding API Client Class
  *
- * @package     ArrayPress/Utils
+ * @package     ArrayPress\Google\Geocoding
  * @copyright   Copyright (c) 2024, ArrayPress Limited
  * @license     GPL2+
  * @version     1.0.0
